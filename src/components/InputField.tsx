bfaac983a6ad243b8ed0aeb61a5c04c5a7bec9d6
shibli21 <syedshiblimahmud@gmail.com>
2020-09-15 23:43:06 +0600

    Fix : input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -22,7 +22,7 @@ const InputField: React.FC<InputFieldProps> = ({
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} type={field.name} />
+      <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
